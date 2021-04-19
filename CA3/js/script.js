d2d3ecddff46ba09e79bf4ae46a859087244b3db
Awa555 <awa2025@gmail.com>
2021-04-19 23:54:43 +0100

    Edit index and js

--- CA3/js/script.js
+++ CA3/js/script.js
@@ -5,10 +5,19 @@ function validate() {
     var regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
     if(regex.test(staffPassword)) {
-        alert("valid Staff Password")
+
+        alert("valid Staff Password");
+
+        document.getElementById('Valid').style.visibility = "visible";
+
+        document.getElementById('inValid').style.visibility = "hidden";
     }
 
     else {
         alert("Invalid Staff Password");
+
+        document.getElementById('inValid').style.visibility = "visible";
+
+        document.getElementById('Valid').style.visibility = "hidden";
     }
 }
