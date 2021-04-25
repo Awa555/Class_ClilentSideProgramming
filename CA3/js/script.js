dd5ac4946a440426b837681e8c74fd7f019471b1
Awa555 <awa2025@gmail.com>
2021-04-25 14:31:27 +0100

    Change website style and edit css

--- CA3/js/script.js
+++ CA3/js/script.js
@@ -5,17 +5,14 @@ function validate() {
 
     var regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-    if (regex.test(staffPassword)) {
-
-        alert("valid Staff Password");
+    if (regex.test(staffPassword)) {   
 
         document.getElementById('Valid').style.visibility = "visible";
 
         document.getElementById('inValid').style.visibility = "hidden";
     }
 
-    else {
-        alert("Invalid Staff Password");
+    else {   
 
         document.getElementById('inValid').style.visibility = "visible";
 
@@ -27,9 +24,13 @@ function validate() {
 
 // Customers Information Button
 $(document).ready(function () {
+
     $("#btHide").click(function () {
+
         $("#hide").slideToggle();
+
         $("#btHide").toggleClass("active");
+        
     });
 });
 
@@ -37,7 +38,6 @@ $(document).ready(function () {
 
 // Customers
 $(document).ready(function () {
-
 
     var url = "https://randomuser.me/api/?results=5";
     var u = "";
@@ -71,36 +71,5 @@ $(document).ready(function () {
                 })
             });
     }
-
 });
 
-// $(document).ready(function() {
-
-//     $.ajax({
-//         url: 'https://randomuser.me/api/',
-//         dataType: 'json',
-//         success: function (data) {
-//             console.log(data);
-
-//             const user = data.results[0].user;
-//             var imageTag = user.picture.thumbnail;
-//             var name = user.name.title + '. ' + user.name.first + ' ' + user.name.last;
-//             var email = user.email;
-//             var phone = user.phone;
-//             var age = user.dob.age;
-
-//             //prepare DOM object
-//             var image = $('<img>');
-//             imageTag.prop('src', user.picture.thumbnail);
-
-//             var nameTag = $('<div class = "name">');
-//             nameTag.html(name);
-
-//             var emailTag = $('<a class = "email">');
-//             emailTag.html(email).prop('href', 'mailto:' + email);
-
-//             //insert DOM object into DOM
-//             $('#user').append(imageTag).append(nameTag).append(emailTag);
-//         }
-//     });
-// });
